c98900649f5ce953168bb325b1eb7fd5b4476aeb
manmohan <manmohan@gmail.com>
2024-07-01 15:55:24 +0530

    addDarkMode And also Add a switch to turn on off

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@
 import './App.css';
 import Navbar from './Component/Navbar';
 import TextForm from './Component/TextForm';
+import About from './Component/About';
+import { useState } from 'react';
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode ('dark');
+      document.body.style.backgroundColor ='grey'
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor ='white'
+    }
+  }
   return (
     <>  
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my 3">
-      <TextForm heading='Enter you Comment'/>
+      <TextForm heading='Enter you Comment' mode={mode}/>
+      <About/>
       </div>
     </>
   );
--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -22,6 +22,13 @@ export default function TextForm(props) {
             alert("Failed to copied" +err);
         })
     }
+    const handlePastclick =()=>{
+        navigator.clipboard.readText().then((ClipboardItem)=>{
+            setText(ClipboardItem);
+        }).catch((err)=>{
+            alert("Failed to paste"+err);
+        })
+    }
 
     const handleOnChange = (event) => {
         // console.log("On Change");
@@ -34,7 +41,7 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
             <h1>{props.heading} </h1> 
             <button 
             className="btn btn-primary" 
@@ -44,19 +51,21 @@ export default function TextForm(props) {
 
             <div className="mb-3">
                 {/* <label htmlFor="myBox" className="form-label">Enter Your Comment</label> */}
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'light' ? 'white' : 'gray', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUPclick}>Convert to UPPERCASE</button>
             <button className="btn btn-primary mx-1" onClick={handleLoclick}>Convert to LowerCase</button>
             <button className="btn btn-primary mx-1"  onClick={handleCopyclick}>Copy Clipboard</button>
+            <button className="btn btn-primary mx-1"  onClick={handlePastclick}>Paste hear</button>
             
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
             <h2>Your text Summary</h2>
             <p><b>{text.split(" ").length} </b> words and <b>{text.length}</b> characters</p>
             <p><b>{0.008 * text.split(" ").length}</b> Minuts to read</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
+            <h2  >Preview</h2>
+            <p>{text.length>0?text : "Enter something in the taxt box to Preview IT!"}</p>
+            
         </div>
 
         </>
