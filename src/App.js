6a3280a0292e78bee5820fc8b69ffe860e49e16a
manmohan <manmohan@gmail.com>
2024-07-03 19:32:47 +0530

    Insiative Router IN React

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import Navbar from './Component/Navbar';
 import TextForm from './Component/TextForm';
 import About from './Component/About';
 import Alerts from './Component/Alerts';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -16,36 +22,43 @@ function App() {
     setTimeout(() => {
       setAlert(null);
     }, 6000);
-  }
+  };
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'grey';
       showAlert("Dark mode has been enabled", "success");
-      document.title ='TextUtils - DarkMode'
-      setInterval(()=>{
-        document.title ='TextUtils Made BY Manmohan Singh'
-      },2000);
-      setInterval(()=>{
-        document.title ='Install now'
-      },1500);
+      document.title = 'TextUtils - DarkMode';
+      setTimeout(() => {
+        document.title = 'TextUtils Made BY Manmohan Singh';
+      }, 2000);
+      setTimeout(() => {
+        document.title = 'Install now';
+      }, 3500);
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title ='TextUtils - LightMode'
+      document.title = 'TextUtils - LightMode';
     }
-  }
+  };
 
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alerts alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter your Comment" mode={mode} />
-        <About />
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alerts alert={alert} />
+        <div className="container my-3">
+          <Routes> {/*for using v6 */}
+      {/* /users--> Component-1 
+      /users/home -->--> Componet 2 */}
+            <Route exact path="/about" element={<About />} />
+            <Route path="/"
+             element={<TextForm showAlert={showAlert} heading="Enter your Comment" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
